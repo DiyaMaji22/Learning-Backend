cf59b1acdc8057405a0b35ea562a5131e87ab922
Diya Maji <diyamaji22@gmail.com>
2025-09-18 14:01:51 +0530

    Refactor middleware and update index.ejs form structure

--- learning2.js
+++ learning2.js
@@ -1,14 +1,28 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
-app.set("view engine",'ejs');
+app.set("view engine", 'ejs');
 
-app.get('/',(req,res)=>{
+// app.use((req, res, next) => {
+//     console.log("This is middleware");
+//     const a=2;
+//     const b=3;
+//     console.log(a+b);
+//     return next(); 
+   
+// });
+
+app.get('/',(req,res,next)=>{
+    console.log("this is the middleware running");
+    return next();
+}, (req, res) => {
     res.render('index');
-})
-app.get('/about',(req,res)=>{
-    res.send("this is about page ");
-})
+});
+
+app.get('/about', (req, res) => {
+    res.send("this is about page");
+});
+
 app.listen(8000, () => {
     console.log('Express server running on http://localhost:8000');
 });
