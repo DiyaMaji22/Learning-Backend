a13767cddfd6fb76d4dd5a9b0437eb454031d4c3
Diya Maji <diyamaji22@gmail.com>
2025-09-18 22:10:58 +0530

    Add static file serving, update EJS view, and initialize user model and scripts

--- learning2.js
+++ learning2.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.use(express.static("public"))
 app.set("view engine", 'ejs');
 
 // app.use((req, res, next) => {
@@ -16,10 +16,7 @@ app.set("view engine", 'ejs');
    
 // });
 
-app.get('/',(req,res,next)=>{
-    console.log("this is the middleware running");
-    return next();
-}, (req, res) => {
+app.get('/',(req, res) => {
     res.render('index');
 });
 
