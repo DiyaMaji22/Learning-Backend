65c6cc6764fd3f959bc5dc02f438c08a58a65cf6
Diya Maji <diyamaji22@gmail.com>
2025-10-14 00:03:07 +0530

    form

--- server/app.js
+++ server/app.js
@@ -2,14 +2,16 @@ const express=require('express');
 const userrouter=require("./routes/userroute")
 const app=express();
 app.set("view engine","ejs");
+app.set('views', __dirname + '/views');
 
 
-app.get('/user',userrouter)
+app.use('/user',userrouter)
 
 
 app.get('/',(req,res)=>{
-    res.send("This is the page");
+    res.send("This is the home page")
 })
+
 
 app.listen(3000,(req,res)=>{
     console.log("The Server is running on port 3000");
--- server/routes/userroute.js
+++ server/routes/userroute.js
@@ -4,5 +4,9 @@ const router=express.Router();
 
 router.get('/test',(req,res)=>{
     res.send("user Test Route");
+});
+router.get('/register',(req,res)=>{
+
+    res.render('register')
 })
-module.exports=router;
+module.exports=router;  
